b09580592419058b1d2fbc1956e7abf2cdf21efe
Xiao He <praguewatermelon@gmail.com>
2013-07-09 12:10:15 -0700

    Update sampleDist.R

--- functions/sampleDist.R
+++ functions/sampleDist.R
@@ -1,43 +1,38 @@
-sampleDist <- function( data, plotit=TRUE, seed=501 ) {
+sampleDist <- function( FUN, sam_size=200, niter=2000, seed=501, plotit=TRUE,...) {
     #This function creates plots for the sampling distributions of the mean, median, and 20%
     #trimmed mean and outputs the standard errors of the mean, median, and 20% trimmed mean.
-    #It's used to demonstrate effects of distributions on the sample mean, median and trimmed mean.
-    
-    if(seed)    #set the seed so that we can replicate the result.
-      set.seed(seed)  
-	  temp  <- sample( data, 50000, replace=TRUE )	#random sampling
-	  mat   <- matrix( temp, ncol=100 )
+    #It can be used to demonstrate the effect of non-normality on different location estimators. 
 
-	  means   <- colMeans(mat)
-	  medians <- apply( mat, 2, median )
-	  tmeans  <- apply( mat, 2, mean, tr=.2 )	
+    	if(seed)
+		set.seed(501)		#set the seed so that we can replicate the result.
+
+	mat  <- matrix( FUN( sam_size*niter, ...), ncol=niter )
+	means   <- apply( mat, 2, mean )				#creating sampling distributions
+	medians <- apply( mat, 2, median )
+	tmeans  <- apply( mat, 2, mean, tr=.2 )	
 	
-	  if(plotit){
-		  smeans.dens   <- density( means )			#densities
-		  smedians.dens <- density( medians )
-		  stmeans.dens  <- density( tmeans )
+	if(plotit){
+		smeans.dens   <- density( means )			#densities
+		smedians.dens <- density( medians )
+		stmeans.dens  <- density( tmeans )
 	
-		  xlimits <- range( smeans.dens$x, smedians.dens$x, stmeans.dens$x )
-		  ylimits <- range( smeans.dens$y, smedians.dens$y, stmeans.dens$y )
+		xlimits <- range( smeans.dens$x, smedians.dens$x, stmeans.dens$x )
+		ylimits <- range( smeans.dens$y, smedians.dens$y, stmeans.dens$y )
 	
-		  plot( smeans.dens, 
-            xlim = xlimits, 
-            ylim = ylimits, 
-            col="darkgreen", 
-            main="Sampling  Distributions" )
-		
-      lines( smedians.dens, col="red" )
-		  lines( stmeans.dens, col="darkblue" )
+		plot( smeans.dens, xlim=xlimits, ylim=ylimits, col="darkgreen", 
+			  main="Sampling Distributions" )
+		lines( smedians.dens, col="red")
+		lines( stmeans.dens, col="darkblue")
 	
-		  legend( "topright",
-              c("Sample means", "Sample medians", "Sample tr. means"), 
-              col=c("darkgreen", "red","darkblue"), lty=1, bty="n")
-  }
+		legend( "topright", 
+			bty ="n",
+			c("Samp means", "Samp medians", "Samp tr. means"), 
+			col=c("darkgreen", "red","darkblue"), 
+			lty=1)
+	}
 	
 	mean.se   <- round( sd(means), 4 )
 	median.se <- round( sd(medians), 4 )
 	tmean.se  <- round( sd(tmeans), 4 )
-	data.frame(SE=c(mean.se, median.se, tmean.se), 
-             row.names=c("Mean", "Median", "Tmean"))	                      
-
+	data.frame(SE=c(mean.se, median.se, tmean.se), row.names=c("Mean", "Median", "Tmean"))	                      
 }
