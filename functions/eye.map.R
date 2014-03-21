a9d333b29e0142fa0dd325b69dacf85c656bb883
Xiao He <praguewatermelon@gmail.com>
2014-03-20 19:34:49 -0700

    Update eye.map.R

--- functions/eye.map.R
+++ functions/eye.map.R
@@ -7,13 +7,12 @@ eye.map <- function(image.source, data, xvar, yvar, y0top=TRUE, xlab=NULL, ylab=
 	if( y0top ){
 		revpoint <- lim[2]/2
 		data[[yvar]] <- ifelse(data[[yvar]] > revpoint, revpoint - (data[[yvar]] - revpoint), 
-							        ifelse( data[[yvar]] < revpoint, lim[2] - data[[yvar]], revpoint))
+					ifelse( data[[yvar]] < revpoint, lim[2] - data[[yvar]], revpoint))
 	}
 	m <- ggplot(data, aes_string(x=xvar, y=yvar), height=lim[2], width=lim[1]) + 
 		 coord_cartesian(xlim=c(0, lim[1]), ylim=c(0, lim[2])) + 
 		 annotation_raster(image, xmin=-Inf, ymin=-Inf, xmax=Inf, ymax=Inf) 
-	m <- m + 
-		 stat_density2d(aes(alpha=..level..), geom = geom) +
+	m <- m + stat_density2d(aes(alpha=..level..), geom = geom) +
 		 theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank()) + 
 		 theme(panel.background = element_rect(fill="transparent", colour="black")) +
 		 xlab(xlab) + ylab(ylab) + labs(title=title)
